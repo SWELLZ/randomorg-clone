cff19acca134ecd4e213b07274870376a3415121
SWELLZ <69326160+SWELLZ@users.noreply.github.com>
2022-08-07 22:07:35 -0700

    Add list functionality
    adds the functionality of typing in a list, breaking each item with a new line, and saving the list to be displayed (later to be shuffled)

--- src/components/lists/Lists.js
+++ src/components/lists/Lists.js
@@ -1,12 +1,32 @@
 import React, {useState} from "react";
 
+//Handles the list functionality
 const Lists = () => {
+    //initialize the final list and textarea text variables
     const [list, setList] = useState([]);
+    const [text, setText] = useState({
+        visible: false, //This is the list doesn't show until you press shuffle
+        text: ''
+    });
     
     function handleInput(e){
-        setList(e.target.value.split(','))
-        console.log(list)
-        console.log(typeof(list))
+        setText(prevState => ({
+            ...prevState,
+            text: e.target.value,
+        }))
+    }
+
+    //sets visibility to true and sets the list breaking it up by new lines
+    function handleSubmit(){
+        var newText = Object.assign({}, text); //makes copy of text object
+
+        setList(newText.text.split('\n'));
+        setText(prevState => ({...prevState, visible: true}))
+    }
+
+    //TEMPORARY
+    function hideList(){
+        setText(prevState => ({...prevState, visible: false}))
     }
 
     return (
@@ -15,12 +35,16 @@ const Lists = () => {
                 <h1>Enter List Here</h1>
                 <textarea
                     id="list-input"
-                    rows="30"
+                    rows="20"
                     cols="50"
-                    value={list}
+                    value={text.text}
                     onChange={handleInput}
                 ></textarea>
-                <p>{list}</p>
+                <button onClick={handleSubmit}>Submit</button>
+                <button onClick={hideList}>Set to false</button>
+                <div>
+                    {text.visible ? list.map(item => <p>{item}</p>) : <p></p>}
+                </div>
             </div>
         </div>
     )
